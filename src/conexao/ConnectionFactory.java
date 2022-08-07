82372a1b19b47df974f30d313ef7972de43c58a7
k3vinrich4rd <kevinrichardve@gmail.com>
2022-08-07 19:03:11 -0300

    Inserindo atualizações feitas no projeto

--- src/conexao/ConnectionFactory.java
+++ src/conexao/ConnectionFactory.java
@@ -1,31 +1,71 @@
 package conexao;
 
 import java.sql.Connection;
+//Representa a conexão com o banco de dados e proporciona informações sobre as tabelas do banco
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+// Responsável pelo gerenciamento de Drivers e estabelece conexões ao banco de dados
 
 public class ConnectionFactory {
     //Informações referentes a conexão ao banco de dados:
-
     String url = "jdbc:postgresql://localhost:5432/money";
     String user = "postgres";
     String senha = "123456";
     public static Connection connect;
 
-    //Representa a conexão com o banco de dados e proporciona informações sobre as tabelas do banco
-    public Connection getConnect(){
+
+    /*
+    1.ºParte:
+    //Criação do método para iniciar e estabelecer conexões com o banco de dados
+     */
+    public Connection getConnect() {
         try {
             Class.forName("org.postgresql.Driver");
-            connect = DriverManager.getConnection(url,user,senha);
-            if (connect != null){
+            connect = DriverManager.getConnection(url, user, senha);
+            if (connect != null) {
                 System.out.println("Conectado");
-            }else {
+            } else {
                 System.out.println("Não conectado");
             }
-        }catch (Exception e) {
+        } catch (Exception e) {
             System.out.println("Erro de conexão " + e);
             e.printStackTrace();
         }
         return connect;
     }
+
+    /*
+    2.º Parte:
+    Criação do método, para acrescentar valores na tabela do banco de dados escolhido
+     */
+    public int adicionar(String sql) {
+        try {
+            Statement stmt = connect.createStatement();
+            int resposta = stmt.executeUpdate(sql);
+            connect.close();
+            return resposta;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    /*
+    3.º Parte:
+    Criação do método, para fazer uma busca na sua tabela e retornas os valores já adicionados
+     */
+    public ResultSet buscaEstado(String sqlBuscaEstado){
+        try {
+            Statement stmt = connect.createStatement();
+            ResultSet retorno = stmt.executeQuery(sqlBuscaEstado);
+            return retorno;
+
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 
 }
--- src/conexao/MainTeste.java
+++ src/conexao/MainTeste.java
@@ -1,9 +1,49 @@
 package conexao;
 
+import java.sql.ResultSet;
+
 public class MainTeste {
     public static void main(String[] args) {
-        ConnectionFactory connect = new ConnectionFactory();
-        connect.getConnect();
+        /*
+        Primeira parte:
+        Verificação se a conexão com o banco está estável
+         se estiver, ao rodar a Main, deve se retornar "Conectado" */
+        ConnectionFactory con = new ConnectionFactory();
+        con.getConnect();
+
+        //Terceira parte -----------------------------------
+        String sqlbuscarEstado = "Select * from estados";
+        ResultSet retorno = con.buscaEstado(sqlbuscarEstado);
+        //Terceira parte -----------------------------------
+
+        /*
+        Segunda parte:
+        Adicionar valores a alguma tabela especifica presente no banco
+         */
+        String sql = "INSERT INTO estados(id_estado, nome_estado, sigla_estado) " +
+                "values(default, 'Pernambuco', 'PE')";
+        int adicionarEstadoConnnect = con.adicionar(sql);
+        if(adicionarEstadoConnnect > 0){
+            System.out.println("Adição de valores realizada");
+        }else {
+            System.out.println("Adição não realizada\n ");
+        }
+
+
+        /*
+        Terceira parte:
+        Exibir valores existentes na coluna da tabela e adicionados
+         */
+        try {
+            while (retorno.next()){
+                int id_estado = retorno.getInt("id_estado");
+                String nomeEstado = retorno.getString("nome_estado");
+                String siglaEstado = retorno.getString("sigla_estado");
+                System.out.println(id_estado + " - " + nomeEstado + " - " + siglaEstado);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
 
     }
